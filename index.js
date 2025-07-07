9f5e570fed81f4b9fcc0dfa6842eda08f31693fc
Victoria Rios <mariavictoria.rios@gmail.com>
2025-07-07 14:35:30 -0300

    Finalizacion de READEME y cambio de ENTRY POINT

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.json({ message: "Recordá que para realizar peticiones debés estar logueado." });
+    res.json({ message: "Recordá que para realizar peticiones debés estar logueado. \n La URL es /users/login" });
 });
 
 
